6f31b756d0dc51f5e7fc2a15eb68b759a548e01b
hussein-elmlah <husseinelmlah11@gmail.com>
2024-05-03 07:24:24 +0300

    fix: author create method

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,7 +21,7 @@ class AuthorsController < ApplicationController
 
   # POST /authors or /authors.json
   def create
-    @author = Author.update(author_params)
+    @author = Author.new(author_params)
 
     respond_to do |format|
       if @author.save
